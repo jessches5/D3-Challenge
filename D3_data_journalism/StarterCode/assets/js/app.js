82496bd106cb7a941ecae5376117522d2e3f5768
Jessica Chester <chesterjessica10@gmail.com>
2021-04-26 16:21:33 -0500

    append axes to the chart

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -47,3 +47,9 @@ d3.csv("assets/data/data.csv").then(function(stateData) {
     // ==============================
     var bottomAxis = d3.axisBottom(xLinearScale);
     var leftAxis = d3.axisLeft(yLinearScale);
+
+    // Step 4: Append Axes to the chart
+    // ==============================
+    chartGroup.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
